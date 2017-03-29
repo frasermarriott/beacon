2f8d742aee57b602c4f1eb27a8f718186324d92e
Fraser Marriott <Fraser Marriott>
2017-03-29 01:35:48 +0100

    Adjusted size of greeting/weather to take up less space. Adjusted css tile sizes.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,44 +4,39 @@
 
 @section('content')
 
+<div id="subnav">
+    <div class="container">
+        <div class="col-md-6">
+            {{-- Check the time of day and display appropriate greeting. --}}
+            @if (Carbon::now()->hour < 12)
+                <h1>Good morning, {{ ucwords(\Auth::user()->name) }}</h1>
+            @elseif (Carbon::now()->hour > 11 && Carbon::now()->hour < 18)
+                <h1>Good afternoon, {{ ucwords(\Auth::user()->name) }}</h1>
+            @elseif(Carbon::now()->hour > 17) 
+                <h1>Good evening, {{ ucwords(\Auth::user()->name) }}</h1>
+            @else
+                <h1>Welcome, {{ ucwords(\Auth::user()->name) }}</h1>
+            @endif
+            
+        </div>
+
+        <div class="col-md-6 dashboard-weather">
+            <div class="hidden">
+                <form id="weather_api">
+                    {{-- hardcoded location temporarily, will get users location from database --}}
+                    <input type="hidden" id="city_name" value="Edinburgh">
+                </form>
+            </div>
+
+            <h1 class="weather"><div class="weather_temp"></div>&deg;C, <div class="weather_desc"></div></h1>
+
+        </div>
+    </div>
+</div>
+
 <div id="dash-fullsize">
 <div class="dashboard-nav">
 
-    <div class="container">
-
-
-        <div class="row">
-            <div class="col-md-6 dashboard-heading">
-                {{-- Check the time of day and display appropriate greeting. --}}
-                @if (Carbon::now()->hour < 12)
-                    <h1>Good morning, {{ ucwords(\Auth::user()->name) }}</h1>
-                @elseif (Carbon::now()->hour > 11 && Carbon::now()->hour < 18)
-                    <h1>Good afternoon, {{ ucwords(\Auth::user()->name) }}</h1>
-                @elseif(Carbon::now()->hour > 17) 
-                    <h1>Good evening, {{ ucwords(\Auth::user()->name) }}</h1>
-                @else
-                    <h1>Welcome, {{ ucwords(\Auth::user()->name) }}</h1>
-                @endif
-                
-            </div>
-
-
-            <div class="col-md-6 dashboard-heading dashboard-weather">
-                <div class="hidden">
-                    <form id="weather_api">
-                        {{-- hardcoded location temporarily, will get users location from database --}}
-                        <input type="hidden" id="city_name" value="Edinburgh">
-                    </form>
-                </div>
-
-                <h1 class="weather"><div class="weather_temp"></div>&deg;C, <div class="weather_desc"></div></h1>
-
-            </div>
-        </div>
-
-    </div>
-
-    
 
 
 <div id="tiles">
@@ -151,42 +146,6 @@
 <div id="dash-mobile">
 <div class="dashboard-nav">
 
-    <div class="container">
-
-
-        <div class="row">
-            <div class="col-md-6 dashboard-heading">
-            <p>mobilenav</p>
-                {{-- Check the time of day and display appropriate greeting. --}}
-                @if (Carbon::now()->hour < 12)
-                    <h1>Good morning, {{ ucwords(\Auth::user()->name) }}</h1>
-                @elseif (Carbon::now()->hour > 11 && Carbon::now()->hour < 18)
-                    <h1>Good afternoon, {{ ucwords(\Auth::user()->name) }}</h1>
-                @elseif(Carbon::now()->hour > 17) 
-                    <h1>Good evening, {{ ucwords(\Auth::user()->name) }}</h1>
-                @else
-                    <h1>Welcome, {{ ucwords(\Auth::user()->name) }}</h1>
-                @endif
-                
-            </div>
-
-
-            <div class="col-md-6 dashboard-heading dashboard-weather">
-                <div class="hidden">
-                    <form id="weather_api">
-                        {{-- hardcoded location temporarily, will get users location from database --}}
-                        <input type="hidden" id="city_name" value="Edinburgh">
-                    </form>
-                </div>
-
-                <h1 class="weather"><div class="weather_temp"></div>&deg;C, <div class="weather_desc"></div></h1>
-
-            </div>
-        </div>
-
-    </div>
-
-    
 
 
 <div id="tiles">
