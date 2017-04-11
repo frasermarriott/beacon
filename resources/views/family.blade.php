9c22cebfa9ee697e2453f6de873d3a098671de74
Fraser <40212234@live.napier.ac.uk>
2017-04-11 15:07:22 +0100

    Added styling to Profile and Family pages.

--- resources/views/family.blade.php
+++ resources/views/family.blade.php
@@ -22,9 +22,62 @@
 </div>
 
 
-<div class="container">
+<div class="container pus-top">
 
 
+  <div class="jumbotron list-content">
+    <ul class="list-group">
+      <li href="#" class="list-group-item title">
+        My Family
+      </li>
+      <br>
+      <li href="#" class="list-group-item text-left">
+        <img class="img-thumbnail" src="http://bootdey.com/img/Content/User_for_snippets.png">
+        <label class="name">
+            Person 1<br>
+        </label>
+        <label class="pull-right">
+            <a  class="btn btn-success btn-xs glyphicon glyphicon-user" href="#" title="View"></a>
+            <a  class="btn btn-danger  btn-xs glyphicon glyphicon-trash" href="#" title="Delete"></a>
+            <a  class="btn btn-info  btn-xs glyphicon glyphicon glyphicon-comment" href="#" title="Send message"></a>
+        </label>
+        <div class="break"></div>
+      </li>
+      <li href="#" class="list-group-item text-left">
+          <img class="img-thumbnail"  src="http://bootdey.com/img/Content/user_1.jpg">
+        <label class="name">
+          Person 2
+        </label>
+        <label class="pull-right">
+            <a  class="btn btn-success btn-xs glyphicon glyphicon-user" href="#" title="View"></a>
+            <a  class="btn btn-danger  btn-xs glyphicon glyphicon-trash" href="#" title="Delete"></a>
+            <a  class="btn btn-info  btn-xs glyphicon glyphicon glyphicon-comment" href="#" title="Send message"></a>
+        </label>
+        <div class="break"></div>
+      </li>
+      <li href="#" class="list-group-item text-left">
+          <img class="img-thumbnail"  src="http://bootdey.com/img/Content/user_2.jpg">
+          <label class="name">
+            Person 3
+          </label>
+        <label class="pull-right">
+            <a  class="btn btn-success btn-xs glyphicon glyphicon-user" href="#" title="View"></a>
+            <a  class="btn btn-danger  btn-xs glyphicon glyphicon-trash" href="#" title="Delete"></a>
+            <a  class="btn btn-info  btn-xs glyphicon glyphicon glyphicon-comment" href="#" title="Send message"></a>
+        </label>
+        <div class="break"></div>
+      </li>
+      {{-- <li href="#" class="list-group-item text-left">
+        <a class="btn btn-block btn-primary">
+            <i class="glyphicon glyphicon-refresh"></i>
+            Load more...
+        </a>
+      </li> --}}
+    </ul>
+  </div>
+  </div>
+
+{{-- 
     <div class="row">
         <div class="col-md-12">
 
@@ -33,7 +86,7 @@
             
         </div>
 
-    </div>
+    </div> --}}
 
 </div>
 @endsection
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -17,12 +17,75 @@
     			<h2>My Profile</h2>
     		</div>
     	
+        </div>
+    </div>
+</div>
+
+
+<div class="container push-top-profile">
+    <div class="row profile-page">
+        <div class="col-md-3">
+            <div class="profile-sidebar">
+                <!-- SIDEBAR USERPIC -->
+                <div class="profile-userpic">
+                    <img src="https://placehold.it/300x300.jpg" class="img-responsive" alt="">
+                </div>
+                <!-- END SIDEBAR USERPIC -->
+                <!-- SIDEBAR USER TITLE -->
+                <div class="profile-usertitle">
+                    <div class="profile-usertitle-name">
+                        Example name
+                    </div>
+{{--                     <div class="profile-usertitle-job">
+                        Grandma
+                    </div> --}}
+                </div>
+                <!-- END SIDEBAR USER TITLE -->
+                <!-- SIDEBAR BUTTONS -->
+                <div class="profile-userbuttons">
+                    {{-- <button type="button" class="btn btn-success btn-sm">Follow</button> --}}
+                    {{-- <button type="button" class="btn btn-danger btn-sm">Message</button> --}}
+                </div>
+                <!-- END SIDEBAR BUTTONS -->
+                <!-- SIDEBAR MENU -->
+                <div class="profile-usermenu">
+                    <ul class="nav">
+                        <li class="active">
+                            <a href="#">
+                            <i class="glyphicon glyphicon-home"></i>
+                            About Me </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                            <i class="glyphicon glyphicon-user"></i>
+                            Account Settings </a>
+                        </li>
+{{--                         <li>
+                            <a href="#" target="_blank">
+                            <i class="glyphicon glyphicon-ok"></i>
+                            Tasks </a>
+                        </li> --}}
+                        <li>
+                            <a href="#">
+                            <i class="glyphicon glyphicon-flag"></i>
+                            Help </a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- END MENU -->
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="profile-content">
+               Some user related content goes here...
+            </div>
+        </div>
     </div>
 </div>
 
 
 
-<div class="messages-container">
+{{-- <div class="messages-container">
 
 
     <div class="row">
@@ -49,5 +112,5 @@ Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et 
 
     </div>
 
-</div>
+</div> --}}
 @endsection
